c3c299899b0ed120ee1efe254dfa64e745fef860
lip ables <phillipables1@gmail.com>
2018-09-02 22:43:11 -0400

    zombieScript-> debug test on update walk forward referse or idle horizontal input, animator refence to component on start

--- LetsDoDrugs/Assets/Toon Zombie S/ZombieScript.cs
+++ LetsDoDrugs/Assets/Toon Zombie S/ZombieScript.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class ZombieScript : MonoBehaviour {
 
+    Animator animZombie;
+
 	// Use this for initialization
 	void Start () {
-		
+        animZombie = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetAxis("Vertical") > 0)
+        {
+            Debug.Log("WAAAAALK!, unity");
+        }
+        else if(Input.GetAxis("Vertical") < 0)
+        {
+            Debug.Log("Reverse, unity");
+        }
+        else
+        {
+            
+        }
 	}
 }
