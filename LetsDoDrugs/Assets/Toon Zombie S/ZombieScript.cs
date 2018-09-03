e16b000b9d4a9845ed37c4b0c6cd99e3217d81b6
lip ables <phillipables1@gmail.com>
2018-09-03 09:40:13 -0400

    player now moves in all four directions and has those inputs tied to its move or dont move animations

--- LetsDoDrugs/Assets/Toon Zombie S/ZombieScript.cs
+++ LetsDoDrugs/Assets/Toon Zombie S/ZombieScript.cs
@@ -5,22 +5,57 @@ using UnityEngine;
 public class ZombieScript : MonoBehaviour {
 
     Animator animZombie;
+    public float walkSpeed = 10.0f;
 
 	// Use this for initialization
 	void Start () {
         animZombie = GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		//if(Input.GetAxis("Vertical") != 0)
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+
+    // Update is called once per frame
+    void Update() {
+        //if(Input.GetAxis("Vertical") != 0)
+        //
+        //Player Animate
+        if (Input.GetKeyDown(KeyCode.UpArrow)
+            || Input.GetKeyDown(KeyCode.DownArrow)
+            || Input.GetKeyDown(KeyCode.RightArrow)
+            || Input.GetKeyDown(KeyCode.LeftArrow)
+            )
         {
             animZombie.SetTrigger("Walk");
         }
-        if(Input.GetKeyUp(KeyCode.UpArrow))
-        //if(Input.GetAxis("Vertical") == 0)
+
+        //Move 
+        //Forward
+        if (Input.GetKey(KeyCode.UpArrow))
         {
+            transform.position += Vector3.forward * Time.deltaTime * walkSpeed;
+        }
+        //Back
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= Vector3.forward * Time.deltaTime * walkSpeed;
+        }
+        //Left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * Time.deltaTime * walkSpeed;
+        }
+        //Right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * Time.deltaTime * walkSpeed;
+        }
+
+        //Stop Animation
+        if (Input.GetKeyUp(KeyCode.UpArrow)
+            || Input.GetKeyUp(KeyCode.DownArrow)
+            || Input.GetKeyUp(KeyCode.RightArrow)
+            || Input.GetKeyUp(KeyCode.LeftArrow)
+            )
+            //if(Input.GetAxis("Vertical") == 0)d
+            {
             animZombie.SetTrigger("Stop");
             Debug.Log("Stopppppppppp't");
         }
