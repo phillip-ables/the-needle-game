ea8c1b715d2549191f3febdf368b13603b1b46ca
lip ables <phillipables1@gmail.com>
2018-09-03 22:16:38 -0400

    wasd controlls added to tranform charachter position and rotation

--- LetsDoDrugs/Assets/Adromena --lady in a dress/AndromedaScript.cs
+++ LetsDoDrugs/Assets/Adromena --lady in a dress/AndromedaScript.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AndromedaScript : MonoBehaviour {
-    Animator animAndromeda;
+    private Animator animAndromeda;
+    public float speed = 5.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +13,15 @@ public class AndromedaScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //wasd controlls
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        //Jumping
         if (Input.GetButtonDown("Jump"))
         {
             animAndromeda.SetTrigger("isJumping");
