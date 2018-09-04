70b16b4fcd9ea07672eed6e982010d8ffd51af52
lip ables <phillipables1@gmail.com>
2018-09-03 21:56:50 -0400

    transitions set up for animation and controller script created to set is jumping trigger when jump button is down

---
+++ LetsDoDrugs/Assets/Adromena --lady in a dress/AndromedaScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndromedaScript : MonoBehaviour {
+    Animator animAndromeda;
+
+	// Use this for initialization
+	void Start () {
+        animAndromeda = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            animAndromeda.SetTrigger("isJumping");
+        }
+	}
+}
